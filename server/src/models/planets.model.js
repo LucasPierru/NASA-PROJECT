891953584f43b966e0789fd570b9963c5b0980dc
LucasPierru <lucaspierru7@gmail.com>
2022-07-28 11:28:43 -0400

    init

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED' 
+          && planet['koi_insol'] > 0.36 
+          && planet['koi_insol'] < 1.11
+          && planet['koi_prad'] < 1.6;
+}
+
+/* 
+const promise = new Promise((resolve, reject) => {
+  resolve(42);
+})
+promise.then((result) => {
+
+});
+const result = await promise;
+*/
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname,'..', '..', 'data', 'kepler_data.csv'))
+    .pipe(parse({
+      comment: '#',
+      columns: true,
+    })) 
+    .on('data', (data) => {
+      if (isHabitablePlanet(data)){
+        habitablePlanets.push(data);
+      }
+    })
+    .on('error', (err) => {
+      console.log(err);
+      reject(err);
+    })
+    .on('end', () => {
+      console.log(`Found ${habitablePlanets.length} habitable planets!`)
+      resolve();
+    });
+  });
+}
+
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
+
+module.exports = {
+  loadPlanetsData,
+  getAllPlanets,
+}
